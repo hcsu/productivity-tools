47d4e37d7c00aa89c3c52668bf442630f972dcfb
Haochuan <hi@hcsu.io>
2022-02-13 17:17:33 +0800

    Update aws-ecs-running-images.sh
    Fix missing first element in output TEMP file

--- aws-ecs-running-images.sh
+++ aws-ecs-running-images.sh
@@ -8,26 +8,29 @@ regions=(
   'ap-southeast-1'
 )
 
+echo "${regions[@]}"
+
 for region in "${regions[@]}"; do
   echo Scaning region "$region"
   clusters=$(aws ecs list-clusters --region "$region" --query 'clusterArns' --output text)
 
   for cluster in $clusters; do
-    echo Scaning clusters...
-    tasks=$(aws ecs list-tasks --region "$region" --cluster="$cluster" --desired-status RUNNING --query taskArns --output text)
-    
-    for task in $tasks; do 
-      echo Scaning tasks...
+    echo Scaning clusters "$cluster"...
+    IFS=" " read -r -a tasks <<< "$(aws ecs list-tasks --region "$region" --cluster="$cluster" --desired-status RUNNING --query taskArns | jq -r '.[]' | tr -s '\n' ' ')"
+    echo "${tasks[@]}"
+
+    for task in "${tasks[@]}"; do 
+      echo Scaning task "$task"...
       aws ecs describe-tasks \
         --region "$region" \
         --cluster "$cluster" \
         --tasks "$task" \
-        --query 'tasks[].{Cluster:clusterArn,Service:group,Image:containers[].image,CPU:cpu,Memory:memory}' \
+        --query 'tasks[].{Cluster:clusterArn,Service:group,Image:containers[].image,TaskDef:taskDefinitionArn,TaskArn:taskArn,CPU:cpu,Memory:memory,CapacityProvider:capacityProviderName}' \
         --output json >> "$TEMP"
     done
   done  
 done
 echo All done!
 
-jq '[inputs|.[]]' "$TEMP"
+jq -n '[inputs|.[]]' "$TEMP"
 rm "$TEMP"
